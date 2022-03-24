d20fbaaf748e37ce2310b118c81756db1ca10642
Luis Garcia <luism.garciac@gmail.com>
2022-03-24 00:18:44 -0300

    Requirement 7
    Requirement 7

--- Assets/Tests/EditMode/BowlingGameKataShould.cs
+++ Assets/Tests/EditMode/BowlingGameKataShould.cs
@@ -131,10 +131,110 @@ public class BowlingGameKataShould
 
     }
 
-    //6° Requerimiento: Si el jugador logra un spare o un strike en el ultimo turno,
-    // obtiene una o dos tiradas mas (respectivamente) de bonificacion. Esas Tiradas cuentan como parte del mismo turno (el decimo).
+    //7° Verificar si estamos en el ultimo Frame
+    //Estimado: 30min
+    //Real: 
 
+    [UnityTest]
 
+    public IEnumerator Verify_If_Arrived_To_Last_Frame()
+    {
+        //Arrange
+        yield return null;
+        BowlingMatch bowlingMatch = new BowlingMatch(10);
+        List<int> rollSequence = new List<int> { 0, 1, 2, 3, 4, 5, 0, 1, 2, 3, 4, 5, 0, 1, 2, 3, 0, 1, 10, 3, 5 };
+
+        //Act
+        bowlingMatch.CalculateMatchPoints(rollSequence);
+
+        //Assert
+        Assert.IsTrue(bowlingMatch.isLastFrame);
+    }
+
+    //7° Requerimiento: Si el jugador logra un strike en el ultimo turno,
+    //obtiene dos tiradas mas de bonificacion. Esas Tiradas cuentan como parte del mismo turno (el decimo).
+    //Estimado: 30min
+    //Real: 21min
+
+    [UnityTest]
+
+    public IEnumerator Have_Two_Additional_Rolls_If_Strike_In_Last_Frame()
+    {
+        //Arrange
+        yield return null;
+        BowlingMatch bowlingMatch = new BowlingMatch(10);
+        List<int> rollSequence = new List<int> { 0, 1, 2, 3, 4, 5, 0, 1, 2, 3, 4, 5, 0, 1, 2, 3, 0, 1, 10, 3, 5 };
+
+        //Act
+        bowlingMatch.CalculateMatchPoints(rollSequence);
+
+        //Assert
+        Assert.AreEqual(18, bowlingMatch.frameList.Last().framePoints);
+
+    }
+
+    //7° Requerimiento: Si el jugador logra un spareen el ultimo turno,
+    //obtiene una tirada mas de bonificacion. Esa Tirada cuenta como parte del mismo turno (el decimo).
+    //Estimado: 10min
+    //Real: 5min
+
+    [UnityTest]
+
+    public IEnumerator Have_One_Additional_Rolls_If_Spare_In_Last_Frame()
+    {
+        //Arrange
+        yield return null;
+        BowlingMatch bowlingMatch = new BowlingMatch(10);
+        List<int> rollSequence = new List<int> { 0, 1, 2, 3, 4, 5, 0, 1, 2, 3, 4, 5, 0, 1, 2, 3, 0, 1, 6, 4, 3};
+
+        //Act
+        bowlingMatch.CalculateMatchPoints(rollSequence);
+
+        //Assert
+        Assert.AreEqual(13, bowlingMatch.frameList.Last().framePoints);
+
+    }
+
+    //8° Requerimiento: Si en las tiradas de bonificación el jugador derriba todos los bolos
+    //el proceso no se repite, no se vuelven a generar mas lanzamientos de bonificación.
+    //Estimado: 5min
+    //Real: 5min
+
+    [UnityTest]
+
+    public IEnumerator Not_Provide_Bonus_Rolls_If_All_Pins_Knocked_In_Bonus_Rolls()
+    {
+        //Arrange
+        yield return null;
+        BowlingMatch bowlingMatch = new BowlingMatch(10);
+        List<int> rollSequence = new List<int> { 0, 1, 2, 3, 4, 5, 0, 1, 2, 3, 4, 5, 0, 1, 2, 3, 0, 1, 6, 4, 10 };
+
+        //Act
+        bowlingMatch.CalculateMatchPoints(rollSequence);
+
+        //Assert
+        Assert.AreEqual(20, bowlingMatch.frameList.Last().framePoints);
+    }
+
+    //Requerimiento: Verificar Puntaje Total
+    //Estimado: 5min
+    //Real: 5min
+
+    [UnityTest]
+
+    public IEnumerator Provide_Total_Score()
+    {
+        //Arrange
+        yield return null;
+        BowlingMatch bowlingMatch = new BowlingMatch(10);
+        List<int> rollSequence = new List<int> { 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10 };
+
+        //Act
+        bowlingMatch.CalculateMatchPoints(rollSequence);
+
+        //Assert
+        Assert.AreEqual(300, bowlingMatch.GetTotalScore());
+    }
 }
 
 public class BowlingMatch
@@ -150,17 +250,21 @@ public class BowlingMatch
     public int throwedRolls;
     public int knockPins;
     public List<Frame> frameList = new List<Frame>();
+    public bool isLastFrame;
 
     public void CalculateMatchPoints(List<int> rollSequence) //  11min  22max  10 
     {
         Frame currentFrame = new Frame();
 
         foreach (var knockedPins in rollSequence)
-        {    
-
-
+        {
             if (frameList.Count != 0)
             {
+                if (isLastFrame) //El error esta en que el frame 9 es el último y no le está dejando agregarse sus segundos 10 puntos al frame 8
+                {
+                    frameList.Last().AddBonusPoint(knockedPins);
+                    continue;
+                }
 
                 if (frameList.Last().isSpare)
                 {
@@ -171,18 +275,40 @@ public class BowlingMatch
                 {
                     frameList.Last().AddBonusPoint(knockedPins);
                 }
-            }
 
+                if (frameList.Count >= 2)
+                {
+                    if (frameList[frameList.Count - 2].isStrike)
+                    {
+                        frameList[frameList.Count - 2].AddBonusPoint(knockedPins);
+                    }
+                }
+            }
             currentFrame.Roll(knockedPins);
 
             if (currentFrame.IsFrameEnded())
             {
                 frameList.Add(currentFrame);
                 currentFrame = new Frame();
-                continue;
-            }
 
+                if (frameList.Count == matchFrames)
+                {
+                    isLastFrame = true;
+                }
+            }
         }
+    }
+
+    public int GetTotalScore()
+    {
+        int totalScore = 0;
+
+        foreach (var frame in frameList)
+        {
+            totalScore += frame.framePoints;
+        }
+
+        return totalScore;
     }
 
 }
